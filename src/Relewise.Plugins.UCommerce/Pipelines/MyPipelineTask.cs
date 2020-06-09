008376944ce34a8660a1689386d431fbc6053eed
Brian Holmgård Kristensen <bhk@vertica.dk>
2020-06-09 21:33:52 +0200

    feat: playing with custom pipelines

---
+++ src/Relewise.Plugins.UCommerce/Pipelines/MyPipelineTask.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Ucommerce.EntitiesV2;
+using Ucommerce.Pipelines;
+
+namespace Relewise.Plugins.UCommerce.Pipelines
+{
+    public class MyPipelineTask : IPipelineTask<PurchaseOrder>
+    {
+        public PipelineExecutionResult Execute(PurchaseOrder subject)
+        {
+            string s = "";
+
+            File.WriteAllText(@"c:\tmp\basket.txt", "test");
+
+            return PipelineExecutionResult.Success;
+        }
+    }
+}
